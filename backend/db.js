22422546c20d617eb31054f55b27cf6772f9e9fd
Ansh Gupta <guptaansh6031@gmail.com>
2024-07-23 16:59:47 +0530

    video 43 completed

---
+++ backend/db.js
@@ -0,0 +1,31 @@
+
+
+
+
+const mongoose = require("mongoose")
+
+const mongo_URI = "mongodb://localhost:27017"               //  database connection string..........
+
+
+const connectToMongo = () => {
+    mongoose.connect(mongo_URI)                  //  connecting with mongodb database.....
+    const db = mongoose.connection                      //  now, object db will wait for the connection event....
+
+
+    db.once("open", () => {                                   //  executes the callback if the connection event gives "open".
+        console.log("YaY!!!, Successfully connected to mongoDB.")
+    })
+
+
+    db.on("error", () => {                                   //  executes the callback if the connection event gives "error".
+        console.log("Error while connecting to mongoDB.")
+    })
+}
+
+
+
+module.exports = connectToMongo
+
+
+
+
