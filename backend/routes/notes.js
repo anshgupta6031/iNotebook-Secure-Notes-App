b8ffb4d3ac927e0dcd083ec3096e73dba0120c66
Ansh Gupta <guptaansh6031@gmail.com>
2024-07-26 16:02:01 +0530

    video 53 completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -64,6 +64,45 @@ router.post('/addnote', fetchuser, [
 
 
 
+//  ROUTE 3 : Update an existing note using : PUT "http://localhost:5000/api/notes/updatenote/:id"  Login required...
+
+router.put('/updatenote/:id', fetchuser, [
+    body("title", "Enter a valid title").isLength({ min: 5 }),
+    body("description", "Description must be at least 5 characters").isLength({ min: 5 }),
+], async (req, res) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+
+    try {
+        const newNote = {}
+        if (req.body.title) newNote.title = req.body.title
+        if (req.body.description) newNote.description = req.body.description
+        if (req.body.tag) newNote.tag = req.body.tag
+
+        let note = await Notes.findById(req.params.id)
+        if (!note) return res.status(404).send("Note not found...")
+
+        //  if the note we are trying to update is of another user.......
+        if (note.user.toString() !== req.user.id) return res.status(401).send("Note not accessable....")        //  (note.user.toString()) = (req.params.id)
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+
+        res.json({note})
+    }
+
+    catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+
+
+
 module.exports = router                             //  exported in order to be able to use for any end point......
 
 
